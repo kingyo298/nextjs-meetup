9bea807f34efcead2ba9aa2212ce47529d287985
奥田啓皓 <>
2022-08-13 12:52:56 +0900

    インポートの記述を修正

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,4 +1,4 @@
-import classes from './MeetupDetail/module.css';
+import classes from './MeetupDetail.module.css';
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
